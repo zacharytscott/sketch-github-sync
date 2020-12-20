a6b6b8d34e42873ff69c80fd3f68e61ddf6ebc10
Zach Scott <me@zach.computer>
2020-12-19 19:33:59 -0500

    Added config view

--- src/configure.js
+++ src/configure.js
@@ -1,6 +1,27 @@
-import sketch from 'sketch'
-// documentation: https://developer.sketchapp.com/reference/api/
+import BrowserWindow from 'sketch-module-web-view';
+import Settings from 'sketch/settings';
 
 export default function() {
-  sketch.UI.message("It's alive 🙌")
+  const options = {
+    identifier: 'config'
+  };
+
+  const browserWindow = new BrowserWindow(options);
+
+  browserWindow.loadURL(require('./config.html'));
+
+  const settings = {
+    token: Settings.settingForKey('token'),
+    repoUrl: Settings.settingForKey('repoUrl')
+  };
+
+  browserWindow.webContents
+    .executeJavaScript(`setSettings(${JSON.stringify(settings)})`);
+
+  browserWindow.webContents.on('settingsSaved', settings => {
+    Settings.setSettingForKey('token', settings.token);
+    Settings.setSettingForKey('repoUrl', settings.repoUrl);
+
+    browserWindow.close();
+  });
 }
